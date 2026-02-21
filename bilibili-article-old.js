1e547c48ad5ab90f9f5cfcc5dc7b867ddc68ae7a
Xyc1596 <xyc159623@163.com>
2026-02-21 23:08:11 +0800

    article_type < 4

--- bilibili-article-old.js
+++ bilibili-article-old.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bilibili专栏自动回到旧版
 // @namespace    http://tampermonkey.net/
-// @version      1.1.1
+// @version      1.2.0
 // @description  Bilibili专栏自动重定向至旧版页面（www.bilibili.com/opus/* -> www.bilibili.com/read/cv*/?opus_fallback=1）
 // @author       Xyc1596
 // @license      MIT
@@ -15,7 +15,7 @@
     if (window.location.href.includes("bilibili.com/opus")) {
         console.log(window.__INITIAL_STATE__);
         const basic = window.__INITIAL_STATE__.detail.basic;
-        if (basic.comment_type == 12) {
+        if (basic.comment_type == 12 && basic.article_type < 4) {
             let new_href = `https://www.bilibili.com/read/cv${basic.rid_str}/?opus_fallback=1`;
             window.location.href = new_href;
         }
